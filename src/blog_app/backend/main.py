8ad6fd229ddd6bd4450211fd1da71c97f7886755
n1ghtmare-dev <oboev3691@gmail.com>
2025-04-20 20:47:05 +0300

    update frontend: add articles, path. update backend: add jinja templates work

--- src/blog_app/backend/main.py
+++ src/blog_app/backend/main.py
@@ -1,28 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
 
+BaseDir = Path(__file__).resolve().parent.parent
+templates = Jinja2Templates(directory=str(BaseDir / "frontend/templates"))
+app.mount(
+    "/static",
+    StaticFiles(directory=str(BaseDir / "frontend/static")),
+    name="static"
+)
+
+
 @app.get("/home/")
-def get_articles():
+def get_articles(request: Request):
     articles = [
         {
             'id': 1,
-            'name': 'first article',
+            'title': 'first article',
             'date': 'August 7, 2024'
         },
         {
             'id': 2,
-            'name': 'second article',
+            'title': 'second article',
             'date': 'August 7, 2024'
         },
         {
             'id': 3,
-            'name': 'third article',
+            'title': 'third article',
             'date': 'August 7, 2024'
         },
-    ] 
-    return articles
+    ]
+    return templates.TemplateResponse("index.html", {"request": request, "articles": articles}) 
 
 
 
